472b633e23085e784a42c43a4c31814dc41beae6
Sorokovsky <Sorokovskys@ukr.net>
2022-06-27 22:24:32 +0300

    upd

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import MyContainer from "../component/MyContainer";
 import MyForm from "../component/MyForm";
 import MainLoyout from "../layouts/MainLoyout";
 import MyInput from "../ui/MyInput";
+import cl from "../styles/Home.module.scss";
 interface IProps {};
 interface IState {};
 class Index extends Component<IProps, IState> {
@@ -16,8 +17,13 @@ class Index extends Component<IProps, IState> {
           <MyContainer>
             <MyForm>
               <label htmlFor="count">Кільксть учасників</label>
-              <MyInput type="number" id="count" placeholder="Введіть кількість учасників"
+              <MyInput 
+              type="number" 
+              id="count" 
+              placeholder="Введіть кількість учасників"
               />
+              <MyInput type="file" classes={[cl.file].join(" ")} id="bg" name="bg"/>
+              <label htmlFor="bg">Оберіть задній фон розіграшу</label>
             </MyForm>
           </MyContainer>
         </MainLoyout>
