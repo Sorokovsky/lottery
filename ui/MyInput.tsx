81894d47f6627e14bb34e59ee5b44b097c93ffb4
Sorokovsky <Sorokovskys@ukr.net>
2022-06-27 18:11:19 +0300

    start input

--- ui/MyInput.tsx
+++ ui/MyInput.tsx
@@ -2,16 +2,18 @@ import { Component } from "react";
 interface IProps{
     type?: string;
     name?: string;
+    placeholder?: string;
 };
 interface IState{}
 class MyInput extends Component<IProps, IState>{
     constructor(props: IProps){
         super(props);
     }
-    render():JSX.Element {
+    render():JSX.Element{
         return(
             <input type={this.props.type ? this.props.type : 'text'} 
-            name={this.props.name ? this.props.name : ''}/>
+            name={this.props.name ? this.props.name : ''}
+            placeholder={this.props.placeholder ? this.props.placeholder : ''}/>
         )
     }
 }
