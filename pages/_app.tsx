6fe89ba11af3df6be28f745dd80ee26704b9eba3
Sorokovsky <Sorokovskys@ukr.net>
2022-07-01 17:06:55 +0300

    start

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
-import type { AppProps } from 'next/app';
-import React from 'react';
-import '../styles/globals.scss';
-interface IState {};
-class MyApp extends React.Component<AppProps, IState>{
-    constructor(props:AppProps){
-      super(props);
-    }
-    render():React.ReactNode {
-      let { Component, pageProps } = this.props;
-      return(
-        <Component {...pageProps}/>
-      )
-    }
+import '../styles/globals.scss'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-export default MyApp;
+
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,10 @@
-import { Component } from "react";
-import MyContainer from "../component/MyContainer";
-import MyForm from "../component/MyForm";
-import MainLoyout from "../layouts/MainLoyout";
-import MyInput from "../ui/MyInput";
-import cl from "../styles/Home.module.scss";
-interface IProps {};
-interface IState {};
-class Index extends Component<IProps, IState> {
-    constructor(props: IProps){
-      super(props);
-      this.state = {};
-    }
-    render():JSX.Element{
-      return(
-        <MainLoyout title={"Створити лотерею для розіграшу"}>
-          <MyContainer>
-            <MyForm>
-              <label htmlFor="count">Кільксть учасників</label>
-              <MyInput 
-              type="number" 
-              id="count" 
-              placeholder="Введіть кількість учасників"
-              />
-              <MyInput type="file" classes={[cl.file].join(" ")} id="bg" name="bg"/>
-              <label htmlFor="bg">Оберіть задній фон розіграшу</label>
-            </MyForm>
-          </MyContainer>
-        </MainLoyout>
-      )
-    }
+import type { NextPage } from 'next';
+const Home: NextPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
-export default Index;
+
+export default Home;
