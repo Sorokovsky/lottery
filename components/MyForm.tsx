cda4d7dbe0ac1554d9c46daccfc0b45e82fe2220
Sorokovsky <Sorokovskys@ukr.net>
2022-07-01 19:48:34 +0300

    start form

---
+++ components/MyForm.tsx
@@ -0,0 +1,15 @@
+import React, { FC, ReactNode } from 'react';
+import { methods } from '../types/methods';
+interface Props{
+    children:ReactNode;
+    action?:string;
+    method?:string;
+}
+const MyForm:FC<Props> = ({children, action = '#', method = methods.GET}:Props):JSX.Element => {
+  return (
+    <form action={action} method={method}>
+        {children}
+    </form>
+  )
+}
+export default MyForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next';
 import Container from '../components/Container';
+import MyForm from '../components/MyForm';
 const Home: NextPage = () => {
   return (
    <Container>
-    
+    <MyForm>
+      
+    </MyForm>
    </Container>
   )
 }
