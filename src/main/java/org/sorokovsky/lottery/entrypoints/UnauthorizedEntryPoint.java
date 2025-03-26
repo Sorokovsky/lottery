6ba7e00c20b97f84a1fb0f0e3b1e63aff706b406
Sorokovsky <Sorokovskys@ukr.net>
2025-03-26 15:58:50 +0200

    refactor

--- src/main/java/org/sorokovsky/lottery/entrypoints/UnauthorizedEntryPoint.java
+++ src/main/java/org/sorokovsky/lottery/entrypoints/UnauthorizedEntryPoint.java
@@ -1,9 +1,12 @@
 package org.sorokovsky.lottery.entrypoints;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.sorokovsky.lottery.contract.ApiError;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -13,6 +16,8 @@ public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setHeader(HttpHeaders.WWW_AUTHENTICATE, "Bearer");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        var mapper = new ObjectMapper();
+        var apiError = new ApiError("Unauthorized", HttpStatus.UNAUTHORIZED.value());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, mapper.writeValueAsString(apiError));
     }
 }
