d03c4335b3a629eef3cff605b98b81503cd69e91
Sorokovsky <Sorokovskys@ukr.net>
2025-03-29 14:48:39 +0200

    refactor

--- src/main/java/org/sorokovsky/lottery/config/SecurityConfig.java
+++ src/main/java/org/sorokovsky/lottery/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,7 +27,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+
 public class SecurityConfig {
 
     @Bean
